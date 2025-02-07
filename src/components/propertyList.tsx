0782731fa5aeb3bc46b3da52adc63ad37eb90a7e
odiasesamuel <odiasesamuel4@gmail.com>
2025-02-07 21:33:24 +0100

    improve UI for each property cards

--- src/components/propertyList.tsx
+++ src/components/propertyList.tsx
@@ -27,7 +27,6 @@ export const PropertyList = () => {
   const perPage = Number(searchParams.get("perPage")) || 12;
   const page = Number(searchParams.get("page")) || 1;
 
-  // use Tankstack to fetch and manage
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["propertyList", page, perPage],
     queryFn: ({ signal }) => getProperty({ signal, perPage, page }),
@@ -43,27 +42,28 @@ export const PropertyList = () => {
           {propertyList.map((property: any) => {
             const backgroundImg = property.imageUrls[0];
             return (
-              <Card className="w-[23%] h-[380px] bg-no-repeat bg-center bg-cover relative text-red-700 text-sm border-none shadow-none" style={{ backgroundImage: `url(${backgroundImg})` }} key={property._id}>
+              <Card className="w-[23%] h-[380px] bg-no-repeat bg-center bg-cover relative text-black text-sm border-none shadow-none" style={{ backgroundImage: `url(${backgroundImg})` }} key={property._id}>
                 <CardHeader>
-                  <CardTitle className="absolute text-xs bg-appGreen px-4 py-2 rounded-full font-normal">{property.forSaleOrRent === "Sale" ? "FOR SALE" : property.forSaleOrRent === "Rent" ? "FOR RENT" : "FOR RENT"}</CardTitle>
+                  <CardTitle className="absolute text-xs bg-appGreen px-4 py-2 rounded-full font-normal text-white">{property.forSaleOrRent === "Sale" ? "FOR SALE" : property.forSaleOrRent === "Rent" ? "FOR RENT" : "FOR RENT"}</CardTitle>
                 </CardHeader>
-                <CardContent className="absolute bottom-12">
-                  <p>{property.name}</p>
+                <CardContent className="w-[90%] absolute left-1/2 transform -translate-x-1/2 bottom-3 bg-white rounded-md p-3">
+                  <p className="text-left font-semibold">{property.name}</p>
                   <div className="flex items-center gap-2 mt-2">
                     <MapPin strokeWidth={1.5} size={20} />
                     <span className="font-light text-xs">{property.area}</span>
                   </div>
-                </CardContent>
-                <CardFooter className="absolute bottom-3 flex justify-between w-full">
-                  <div className="flex w-1/2 gap-2">
-                    <Image alt="icon of bed" src="/images/bed.svg" width={20} height={20} className="" />
-                    <span>{property.numberOfRooms}</span>
-                    <span>|</span>
-                    <Image alt="icon of bed" src="/images/shower.svg" width={20} height={20} className="" />
-                    <span>1</span>
+                  <div className="flex justify-between mt-3">
+                    <p className="font-semibold text-[#EB664E]">{formatCurrency(property.price)}</p>
+                    <div className="flex gap-2">
+                      <Image alt="icon of bed" src="/images/bed.svg" width={20} height={20} className="" />
+                      <span>{property.numberOfRooms}</span>
+                      <span className="text-[#E2E2E2]">|</span>
+                      <Image alt="icon of bed" src="/images/shower.svg" width={20} height={20} className="" />
+                      <span>1</span>
+                    </div>
                   </div>
-                  <p className="font-semibold">{formatCurrency(property.price)}</p>
-                </CardFooter>
+                </CardContent>
+                <CardFooter className="absolute bottom-3 flex justify-between w-full"></CardFooter>
               </Card>
             );
           })}
