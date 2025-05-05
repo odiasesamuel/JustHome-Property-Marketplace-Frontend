44e6828ad798cdc04ad615f1934f29c833c8dfdb
odiasesamuel <odiasesamuel4@gmail.com>
2025-05-05 22:24:21 +0100

    change backend url to new deployed instance

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://property-marketplace-backend.onrender.com";
+export const BASE_URL = "https://just-home-property-marketplace-backend.onrender.com";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
