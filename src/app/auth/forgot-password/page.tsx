2c405b59895663899a544c732f3486dde671a2d2
odiasesamuel <odiasesamuel4@gmail.com>
2025-05-09 12:01:50 +0100

    implement responsiveness for forgot and reset password form

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 const ForgetPassword = () => {
   return (
     <div className="-mb-10 flex flex-col items-center px-[3.5%]">
-      <Card className="w-[600px]">
+      <Card className="mb-10 w-[350px] 2xs:w-[380px] xs:w-[460px] sm:mb-0 sm:w-[600px]">
         <CardHeader>
           <CardTitle>Reset your password</CardTitle>
           <CardDescription>Enter your email address and we would send you instructions to reset your password</CardDescription>
--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 const ResetPassword = () => {
   return (
     <div className="-mb-10 flex flex-col items-center px-[3.5%]">
-      <Card className="w-[600px]">
+      <Card className="mb-10 w-[350px] 2xs:w-[380px] xs:w-[460px] sm:mb-0 sm:w-[600px]">
         <CardHeader>
           <CardTitle>Reset your password</CardTitle>
           <CardDescription>Enter your new password to reset your password</CardDescription>
