32bd30554664d55ba5b7e1c2400ad03dbe3083fe
odiasesamuel <odiasesamuel4@gmail.com>
2025-05-12 23:01:32 +0100

    fix pagination layout on small screens

--- src/components/paginationComponent.tsx
+++ src/components/paginationComponent.tsx
@@ -29,44 +29,46 @@ export const PaginationComponent: React.FC<PaginationProps> = ({ page, perPage, 
           <PaginationPrevious href={page > 1 ? `?page=${page - 1}&perPage=${perPage}&${params.toString()}` : "#"} className={page === 1 ? "pointer-events-none opacity-50" : ""} />
         </PaginationItem>
 
-        <PaginationItem>
-          <PaginationLink href={`?page=1&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${page === 1 ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
-            1
-          </PaginationLink>
-        </PaginationItem>
-
-        {page > 3 && (
+        <div className="hidden gap-2 sm:flex">
           <PaginationItem>
-            <PaginationEllipsis />
+            <PaginationLink href={`?page=1&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${page === 1 ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
+              1
+            </PaginationLink>
           </PaginationItem>
-        )}
 
-        {[...Array(pageCount)].map((_, index) => {
-          const pageNum = index + 1;
-          if (pageNum === 1 || pageNum === pageCount) return null;
+          {page > 3 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
 
-          if (pageNum >= page - 2 && pageNum <= page + 2) {
-            return (
-              <PaginationItem key={pageNum}>
-                <PaginationLink href={`?page=${pageNum}&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${pageNum === page ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
-                  {pageNum}
-                </PaginationLink>
-              </PaginationItem>
-            );
-          }
-        })}
+          {[...Array(pageCount)].map((_, index) => {
+            const pageNum = index + 1;
+            if (pageNum === 1 || pageNum === pageCount) return null;
 
-        {page < pageCount - 2 && (
+            if (pageNum >= page - 2 && pageNum <= page + 2) {
+              return (
+                <PaginationItem key={pageNum}>
+                  <PaginationLink href={`?page=${pageNum}&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${pageNum === page ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
+                    {pageNum}
+                  </PaginationLink>
+                </PaginationItem>
+              );
+            }
+          })}
+
+          {page < pageCount - 2 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+
           <PaginationItem>
-            <PaginationEllipsis />
+            <PaginationLink href={`?page=${pageCount}&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${page === pageCount ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
+              {pageCount}
+            </PaginationLink>
           </PaginationItem>
-        )}
-
-        <PaginationItem>
-          <PaginationLink href={`?page=${pageCount}&perPage=${perPage}&${params.toString()}`} className={`rounded-md px-3 py-2 ${page === pageCount ? "bg-appGreen font-bold text-white" : "hover:bg-gray-200"}`}>
-            {pageCount}
-          </PaginationLink>
-        </PaginationItem>
+        </div>
 
         <PaginationItem>
           <PaginationNext href={page < pageCount ? `?page=${page + 1}&perPage=${perPage}&${params.toString()}` : "#"} className={page === pageCount ? "pointer-events-none opacity-50" : ""} />
